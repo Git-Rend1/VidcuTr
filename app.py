3b99f3fb4c707c7a13a2f1da5533c2baa665de8d
Git-Rend1 <gitclo1@techengine.eu.cc>
2026-07-07 17:51:29 +0330

    change app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import tempfile
 import yt_dlp
 from flask import Flask, request, send_file, render_template_string
 from yt_dlp import YoutubeDL
+from yt_dlp.utils import download_range_func
 
 app = Flask(__name__)
 
